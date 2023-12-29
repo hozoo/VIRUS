b053401c33f39629f4ace6c2844be4b726c9becd
hozoo <143112689+hozoo@users.noreply.github.com>
2023-12-29 19:07:21 +0700

    VIRUS
    git clone https://github.com/hozoo/VIRUS/upload
    cd VIRUS
    ls
    sh VIRUS.sh

---
+++ VIRUS.sh
@@ -0,0 +1,240 @@
+#!/bin/bash
+#virus 
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo "\033[31;1m"|lolcat
+echo "\033[31;1m                           
+███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█
+████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█
+███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█
+████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█
+███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+█████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+█████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+█████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██
+█████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██
+█████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███
+██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███
+███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████
+███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████
+████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████
+█████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████
+██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████
+███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████
+██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████
+███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████
+================================================
+=        SYSTEM UMBRELLA DRAK999               =
+=                VIRUS                         =
+=================================================
+=[1].TRACK IP         [6].web whatsappp ban    =
+=[2].Vidio drak       [7].HACKING 40           =
+=[3].brechead from    [8].ddos                 =
+=[4].web hacking      [9].kandura warning      =
+=[5].web osint        [10].deface              =
+=[11].kandura osint   [16].KALI NETHUNTER      =
+=[12].sandmap         [17].UBUNTU              =
+=[13].data base       [18].DEBIAN              =
+=[14].bit coin        [19].PARROT              =
+=[15].hack wifi       [20].RDP 7 TOOLS         =
+=[21].HACKING ATM    =
+=[22].HACKING BANK   =       
+=[23].HACKING BCA    =  
+=[24].HACKING BNI    =
+=[25].HACKING VISA   = 
+=======================
+"|lolcat
+echo "\033[33;1m"
+read -p "  SYSTEM UMBRELLA DRAK999  " c;
+
+if [ $c = 1 ]
+then
+clear
+echo $red"
+███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█
+████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█
+███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█
+████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█
+███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+█████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+█████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+█████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██
+█████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██
+█████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███
+██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███
+███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████
+███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████
+████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████
+█████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████
+██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████
+███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████
+██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████
+███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████
+
+
+
+  
+
+
+
+
+
+"
+                                         
+ 
+
+echo "ip address : "
+read ip
+
+echo
+
+printf '      Ip             :       '
+      curl -s https://ipinfo.io/$ip/json | jq -r '.ip'
+printf '      Country        :       '
+      curl -s https://ipapi.co/$ip//json | jq -r  '.country_name'
+printf '      Region         :       '
+      curl -s https://ipapi.co/$ip//json | jq -r  '.region'
+printf '      City           :       '
+      curl -s https://ipapi.co/$ip//json | jq -r '.city'
+printf '      Postal Code    :       '
+      curl -s https://ipapi.co/$ip//json | jq -r  '.postal'
+echo
+
+printf '      Location       :       '
+      curl -s https://ipinfo.io/$ip/json | jq -r  '.loc'
+
+
+echo
+printf '      Isp            :       '
+     curl -s https://ipinfo.io/$ip/json | jq -r  '.org'
+
+echo
+
+printf '      Timezone       :       '
+     curl -s https://ipinfo.io/$ip/json | jq -r '.timezone'
+printf '      Currency       :       '
+     curl -s https://ipapi.co/$ip//json | jq -r  '.currency'
+printf '      Calling code   :       '
+     curl -s https://ipapi.co/$ip//json | jq -r  '.country_calling_code'
+
+echo
+
+
+fi
+
+
+if [ $c = 2 ]
+then
+xdg-open https://youtu.be/7rwDcTRjYIA?si=9ygmerjhAanqKXOm
+fi
+if [ $c = 3 ]
+then
+xdg-open https://breachforums.is/
+fi
+if [ $c = 4 ]
+then
+xdg-open https://hackertyper.net/
+fi
+if [ $c = 5 ]
+then
+xdg-open https://www.truecaller.com/id-id
+fi
+if [ $c = 6 ]
+then
+xdg-open https://www.whatsapp.com/contact/?subject=messenger
+fi
+if [ $c = 7 ]
+then
+xdg-open https://github.com/htr-tech/zphisher
+fi 
+if [ $c = 8 ]
+then
+xdg-open https://github.com/hozoo/Ddos-HOZOO-DRAK/blob/main/ddos-attack.py
+fi
+if [ $c = 9 ]
+then
+xdg-open https://github.com/hozoo/Kandura-warning-/blob/main/KANDURA-WARNING.py
+fi
+if [ $c = 10 ]
+then
+xdg-open https://github.com/hozoo/DEFACE2024
+fi
+if [ $c = 11 ]
+then
+xdg-open https://github.com/hozoo/Oisnt-hozoo
+fi
+if [ $c = 12 ]
+then
+xdg-open https://github.com/trimstray/sandmap
+fi
+if [ $c = 13 ]
+then
+xdg-open https://github.com/THEPEACEMAKER/BASH-Database-Management-System
+fi
+if [ $c = 14 ]
+then
+https://github.com/8go/coinbash
+fi
+if [ $c = 15 ]
+then
+https://github.com/BadKiko/Tsunami-Fi
+fi
+if [ $c = 16 ]
+then
+xdg-open https://www.kali.org/docs/nethunter/nethunter-rootless/
+fi
+if [ $c = 17 ]
+then
+xdg-open https://github.com/modded-ubuntu/modded-ubuntu
+fi
+if [ $c = 18 ]
+then
+xdg-open https://github.com/mikeroyal/Debian-Guide
+fi
+if [ $c = 19 ]
+then
+xdg-open https://github.com/Dimaslg/osintBOX
+fi
+if [ $c = 20 ]
+then
+xdg-open https://github.com/OnlineHacKing/Cloud_Linux_RDP
+fi
+if [ $c = 21 ]
+then
+xdg-open https://github.com/kerolloz/atm-project
+
+if [ $c = 22 ]
+then
+xdg-open https://github.com/rioastamal/shbca
+
+if [ $c = 23]
+then
+xdg-open https://github.com/rioastamal/shbca
+fi
+if [ $c = 24]
+then
+xdg-open https://github.com/back2arie/BNI-SMS-Banking
+
+if [ $c = 25]
+then
+xdg-open https://gist.github.com/BlackPunk/66a9e123b40b56447fc5ae1d43ba1583
+fi
